a31a48e5984688b7b1a1c3989537140f43e1463e
Ritesh Sarathe <137711361+RiteshDataLab@users.noreply.github.com>
2023-10-03 14:34:41 +0530

    Add files via upload

---
+++ resume_Assignment/certificate.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Certificate Showcase</title>
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- Custom CSS -->
+    <style>
+        body {
+            background-color: #f8f9fa;
+            background-image: url('bg.avif'); 
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-position: center center;
+        }
+        .certificate-card {
+            margin: 20px;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 10px;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+        }
+        .certificate-card img {
+            max-width: 100%;
+            height: auto;
+        }
+        .certificate-title {
+            color: #007BFF; /* Blue color for the certificate titles */
+        }
+        .note-container {
+            background: linear-gradient(45deg, #ff0099, #9900ff);
+            border-radius: 20px;
+            padding: 20px;
+            text-align: center;
+            box-shadow: 0 6px 12px rgba(0, 0, 0, 0.2);
+            margin-top: 20px;
+        }
+        .note-text {
+            font-size: 24px;
+            color: white;
+            font-weight: bold;
+            text-transform: uppercase;
+            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1 class="mt-5 mb-4">My Certificates</h1>
+        <div class="row">
+            <!-- Certificate 1 -->
+            <div class="col-md-6">
+                <div class="certificate-card">
+                    <img src="certificate1.png" alt="Certificate 1">
+                    <h3 class="certificate-title">Cyber Security & Blockchain</h3>
+                    <p>Workshop on Cyber Security & Blockchain</p>
+                    <p>Date: September 24, 2022</p>
+                </div>
+            </div>
+            <!-- Certificate 2 -->
+            <div class="col-md-6">
+                <div class="certificate-card">
+                    <img src="Screenshot 2023-09-08 225735.png" alt="Certificate 2">
+                    <h3 class="certificate-title">Data Analysis using Python</h3>
+                    <p>Successfully completed and scored passing marks in Data Analysis using Python</p>
+                    <p>Date: December 08, 2022</p>
+                </div>
+            </div>
+            <!-- Certificate 3 -->
+            <div class="col-md-6">
+                <div class="certificate-card">
+                    <img src="certificate2.png" alt="Certificate 3">
+                    <h3 class="certificate-title">Foundation to AI Data Science & Data Analytics</h3>
+                    <p>Successfully completed and scored passing marks in Foundation to AI Data Science & Data Analytics.</p>
+                    <p>Date: December 12, 2022</p>
+                </div>
+            </div>
+            <!-- Certificate 4 -->
+            <div class="col-md-6">
+                <div class="certificate-card">
+                    <img src="Screenshot 2023-09-08 225822.png" alt="Certificate 4">
+                    <h3 class="certificate-title">R Programming for Data Science</h3>
+                    <p>Successfully completed and scored passing marks in R Programming for Data Science.</p>
+                    <p>Date: April 16, 2023</p>
+                </div>
+            </div>
+            <!-- Certificate 5 -->
+            <div class="col-md-6">
+                <div class="certificate-card">
+                    <img src="Screenshot 2023-09-08 225850.png" alt="Certificate 5">
+                    <h3 class="certificate-title">Statistical Data Analysis using Python</h3>
+                    <p>Successfully completed and scored passing marks in Statistical Data Analysis using Python</p>
+                    <p>Date: April 16, 2023</p>
+                </div>
+            </div>
+            <!-- Add more certificates as needed -->
+        </div>
+    </div>
+
+    <!-- Custom Note -->
+    <div class="container">
+        <div class="note-container">
+            <p class="note-text">"To my greatest champion and source of inspiration, my beloved father. Each certificate I hold is a tribute to your unwavering support, love, and belief in my dreams. Your guidance and encouragement have shaped my journey, and I'm forever grateful. These certificates are not just mine; they are a reflection of your dedication and sacrifices. Thank you for being my guiding light on this incredible journey of learning and achievement."</p>
+        </div>
+    </div>
+
+    <!-- Bootstrap JS and jQuery CDN (optional) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</
---
+++ resume_Assignment/index.php
@@ -0,0 +1,112 @@
+<?php include('includes/header.php'); ?>
+<?php include('navbar.php'); ?>
+
+<?php include('slider.php'); ?>
+
+<section class="section" id="about">
+    <div class="container">
+        <div class="row">
+        <h4 style="text-align: center;">About Us</h4>
+    
+            <div class="col-md-4">
+                <br>
+                <img class="w-100" src="images/user.jpg" />
+
+
+
+            </div>
+            <div class="col-md-8">
+            <br>
+                <br>
+                <p>Pursuing a B.Tech degree in Artificial Intelligence at Sage University, Bhopal, has been an exhilarating journey that has allowed me to immerse myself in the captivating world of cutting-edge technology. This academic pursuit isn't just a stepping stone in my career; it is a testament to my profound passion for Artificial Intelligence and data analytics, a passion that has been the driving force behind my educational endeavors.
+<br>
+<br>
+From the very beginning, the realm of Artificial Intelligence has fascinated me. Its potential to transform industries, improve lives, and solve complex problems is nothing short of astounding. This fascination ignited a spark within me, propelling me towards a deep and unwavering commitment to this field.
+<br>
+<br>
+As a dedicated student, I have not only embraced the structured curriculum but have also taken the initiative to explore beyond the confines of the classroom. I have immersed myself in books, research papers, and online courses, striving to stay on the cutting edge of AI developments. This self-directed learning has not only expanded my knowledge but has also honed my critical thinking and problem-solving skills.
+<br>
+<br>
+One of the most exciting aspects of my journey has been my exploration of data analytics. I consider myself a Data Analytics Enthusiast, and my passion for this field is as fervent as my love for Artificial Intelligence. I am captivated by the idea of extracting meaningful insights from vast datasets, transforming raw information into actionable knowledge. This enthusiasm has led me to delve deep into statistical analysis, machine learning algorithms, and data visualization techniques. I have also had the privilege of working on several real-world projects, where I have applied data analytics to solve practical problems. These experiences have not only reinforced my theoretical knowledge but have also provided me with valuable hands-on skills.
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section" id="education">
+    <div class="container">
+        <div class="row">
+        <div class="col-md-12">
+        <h4 style="text-align: center;"><b>EDUCATION</b></h4>
+        <br>
+        <br>
+        <p>
+        <b>Sage University, Bhopal
+
+Expected Graduation: 2025
+Bachelor of Technology (B.Tech) in Artificial Intelligence
+Pursuing a Bachelor of Technology (B.Tech) in Artificial Intelligence at Sage University, Bhopal, has been a transformative and intellectually stimulating journey. This comprehensive program has allowed me to delve deep into the world of Artificial Intelligence, a field that I am profoundly passionate about. The curriculum covers a broad range of AI subjects, including machine learning, neural networks, natural language processing, computer vision, and ethics in AI. I have had the privilege of learning from experienced faculty members who are experts in their respective fields.
+<br>
+<br>
+What makes this academic journey truly remarkable is the emphasis on practical application. I have engaged in hands-on projects that have challenged me to apply theoretical knowledge to real-world problems. These projects have not only expanded my technical skills but have also instilled problem-solving abilities and a keen analytical mindset. Moreover, collaborating with fellow students on these projects has fostered a collaborative and innovative learning environment.
+<br>
+<br>
+<b>Kendriya Vidyalaya, Seoni Malwa</b>
+
+Year of Completion: 2021
+Higher Secondary Certificate (HSC)
+My journey in education began at Kendriya Vidyalaya, Seoni Malwa, where I completed my Higher Secondary Certificate (HSC) in 2021. This phase of my education was instrumental in shaping my academic foundation. Scoring an impressive 90% in the Class 12 examinations conducted by the Central Board of Secondary Education (CBSE) not only highlighted my dedication to academic excellence but also reinforced my strong work ethic and time management skills.
+
+The curriculum at Kendriya Vidyalaya provided a well-rounded education, encompassing subjects that laid the groundwork for my future academic pursuits. It was during this period that I developed a passion for technology and data analysis, which ultimately led me to pursue a B.Tech degree in Artificial Intelligence.
+        </p>
+
+        </div>
+        </div>
+    </div>
+    </div>
+</section>
+
+
+
+<section class="section" id="skills">
+    <div class="container">
+        <div class="row">
+        <div class="col-md-12">
+            <br>
+            <br>
+        <h4 style="text-align: center;"><b>Work Experience</b></h4>
+        <br>
+        <br>
+        <p>
+            <h4>Instagram Data Web Scraping:</h4>
+<br>
+I successfully executed a web scraping project focused on Instagram, extracting and analyzing valuable data. This showcased my expertise in data acquisition techniques, allowing me to gather insights from a diverse dataset.
+<br>
+<br>
+<h4>Machine Learning Models:</h4>
+<br>
+Throughout my academic journey, I developed and implemented diverse machine learning models. These projects highlighted my problem-solving abilities and a deep understanding of AI concepts, including regression, classification, and clustering.
+<br>
+<br>
+<h4>Commercial Website:</h4>
+I took on the challenge of designing and creating a commercial website, showcasing my web development skills. The project emphasized my creative approach to user-friendly design, resulting in an engaging and aesthetically pleasing online platform.</p>
+<br>
+        </div>
+        </div>
+    </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+<?php
+include('includes/footer.php');
+?>
